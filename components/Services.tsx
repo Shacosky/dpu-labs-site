660970b6978f3ff6cd7841087866d8b1cdfc76e3
d3usD <d3usd3v@outlook.com>
2025-12-20 14:35:20 -0300

    arreglando build

--- components/Services.tsx
+++ components/Services.tsx
@@ -92,7 +92,7 @@ export function Services() {
 
       <div className="mt-10 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {ITEMS.map((item, index) => {
-          const { ref, isVisible } = useScrollReveal<HTMLDivElement>();
+          const { ref, isVisible } = useScrollReveal<HTMLButtonElement>();
           return (
             <button
               key={item.key}
